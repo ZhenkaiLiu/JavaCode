ac6921afe4a3a9630c4ea80c15967e6b95aaf22a
ZhenkaiLiu <17361598160@163.com>
2026-04-15 13:51:58 +0800

    包的知识点

---
+++ JavaSE_One/javase.api.day06/src/com/zhenkai/package_demo/PackageDemo.java
@@ -0,0 +1,20 @@
+package com.zhenkai.package_demo;
+
+/**
+ * @author Zhenkai
+ * @date 2026/4/14 周二
+ * @description 讲解导包相关知识
+ *
+ **/
+public class PackageDemo {
+    public static void main(String[] args) {
+        // 包：用来管理不同程序的，类似于文件夹
+        /* 包的相关注意事项：
+        * 1. 在同一个包下的程序可以直接访问
+        * 2. 访问其他包下的程序，必须导包才能使用
+        * 3. 调用 Java 提供的程序，也必须先使用导包才能使用， Java Long 包下的程序可以直接使用
+        * 4. 访问多个其他程序下的包，程序名一样的情况下，默认只能导入一个程序，其他程序需要包名带类名访问
+        */
+
+    }
+}
